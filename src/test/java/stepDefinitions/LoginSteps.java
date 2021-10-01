73924e98d048b4ae2113707777d838f27596c251
amandeep.saluja <amandeepsaluja25@gmail.com>
2021-10-02 01:15:31 +0530

    created dummy test case with cucumber

---
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -0,0 +1,29 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+
+	@Given("^user is on login page$")
+	public void userIsOnLoginPage() {
+		
+	}
+
+	@When("^user enters username and password$")
+	public void userEntersUsernameAndPassword() {
+
+	}
+
+	@And("^clicks on login button$")
+	public void clicksOnLoginButton() {
+
+	}
+
+	@Then("^user is navigated to home page$")
+	public void userIsNavigatedToHomePage() {
+
+	}
+}
